6c37c4535768a467fcfb0ab010b8e65507623ab5
Mohamed Alyan <50428010+alyanmo96@users.noreply.github.com>
2021-02-16 20:26:46 +0200

    Add files via upload

--- app/routes/web.php
+++ app/routes/web.php
@@ -26,10 +26,33 @@ Route::post('/upload_work_photo',"work_photo@store");
 Route::get('/delete_work_video/{id}',"work_videoController@delete");
 Route::get('/remove_photo/{id}',"work_photo@destroy");
 
+
+Route::get('/edit_Synthetic_grass',"work_videoController@show");
+Route::post('/upload_grass',"work_videoController@store");
+Route::get('/grass_edit',"work_videoController@show");
+Route::get('/delete_grass/{id}',"work_videoController@delete");
+Route::get('/grass',"work_videoController@show_for_customers");
+Route::get('/grass',"work_videoController@show_for_customers")->name('grass.show');
+Route::get('/grassAddToCart/{product}',"CartController@add_item_back_to_grass")->name('grass_cart.add');
+
+
+
 Route::get('/message',"messageController@show");
 Route::get('/delete_message/{id}',"messageController@destroy");
 Route::get('/people_buy',"Controller@people_buy_page_admin");
 Route::get('/delete_people_buy/{id}',"Controller@destroy");
+
+Route::get('/edit_product_by_admin/{id}',"PlantController@edit_special_product_by_admin");
+Route::post('/upload_special_plant',"adminController@update_plant");
+Route::get('/edit_grass_by_admin/{id}',"work_videoController@edit_special_grass_by_admin");
+Route::post('/upload_special_grass',"adminController@update_grass");
+Route::get('/edit_equipment_by_admin/{id}',"work_equipmentController@edit_special_work_equipment_by_admin");
+Route::post('/upload_special_work_equipments',"adminController@update_work_equipment");
+Route::get('/edit_gift_by_admin/{id}',"giftController@edit_special_gift_by_admin");
+Route::post('/upload_special_gifts',"adminController@update_gift");
+Route::get('/edit_box_by_admin/{id}',"box@edit_special_product_by_admin");
+Route::post('/upload_special_box',"adminController@update_box");
+
 
 /////////////////////////////////NORMAL USERS///////////////////////////////////////////////////////
 Route::get('/',"Controller@show_for_customers");
@@ -62,10 +85,21 @@ Route::get('/boxs',"box@show_for_customers");
 
 
 
-Route::get('/plant_type_two',"PlantController@show_for_customers_one")->name('plant_type_two.show');
+Route::get('/plant_type_one',"PlantController@show_for_customers_one")->name('plant_type_one.show');
 Route::get('/plant_type_two',"PlantController@show_for_customers_two")->name('plant_type_two.show');
 Route::get('/plant_type_three',"PlantController@show_for_customers_three")->name('plant_type_three.show');
 Route::get('/plant_type_four',"PlantController@show_for_customers_four")->name('plant_type_four.show');
+Route::get('/plant_type_five',"PlantController@show_for_customers_five")->name('plant_type_five.show');
+Route::get('/plant_type_six',"PlantController@show_for_customers_six")->name('plant_type_six.show');
+Route::get('/plant_type_seven',"PlantController@show_for_customers_seven")->name('plant_type_seven.show');
+
+
+Route::get('/box_type_one',"box@show_for_customers_box_one")->name('box_type_one.show');
+Route::get('/box_type_two',"box@show_for_customers_box_two")->name('box_type_two.show');
+Route::get('/box_type_three',"box@show_for_customers_box_three")->name('box_type_three.show');
+
+
+
 
 
 
@@ -78,3 +112,8 @@ Route::get('/giftAddToCart/{product}',"CartController@add_item_back_to_gift")->n
 Route::get('/boxAddToCart/{product}',"CartController@add_item_back_to_box")->name('box_cart.add');
 Route::get('/work_equipmentAddToCart /{product}',"CartController@add_item_back_to_equpment")->name('work_equipment_cart.add');
 Route::get('/delete_from_cart/{id}',"CartController@destroy");
+
+
+Route::any('{any}', function () {//for any other Link
+    return redirect('/');
+});
